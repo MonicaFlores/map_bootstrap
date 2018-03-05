30f69135676239512f174157ff475d5ad3342ff0
mf3435 <mf3435@nyu.edu>
2018-03-04 22:37:47 -0500

    add style

--- js/scripts2.js
+++ js/scripts2.js
@@ -8,6 +8,15 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+/*Test lighter base map*/
+/*var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(map);*/
+
 var places = [
   {
     placeName: '<a href="https://goo.gl/BWyGgr">The Good Room<a>',
